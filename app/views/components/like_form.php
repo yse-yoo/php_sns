30210ac430ffc12e2008fa74211a4895dec89f9b
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-11-13 10:28:14 +0900

    update

--- app/views/components/like_form.php
+++ app/views/components/like_form.php
@@ -1,8 +1,8 @@
 <div class="tweet-nav mb-3">
     <!-- TODO: usersのIDと tweetsのIDを送信 -->
-    <form action="" method="">
-        <input type="hidden" name="" value="<?= $user['id'] ?>">
-        <input type="hidden" name="" value="<?= $tweet['id'] ?>">
+    <form action="like.php" method="POST">
+        <input type="hidden" name="user_id" value="<?= $user['id'] ?>">
+        <input type="hidden" name="tweet_id" value="<?= $tweet['id'] ?>">
 
         <?php if (in_array($tweet['id'], $user_likes)) : ?>
             <button class="btn btn-sm"><img src="../images/svg/heart_active.svg"></button>
